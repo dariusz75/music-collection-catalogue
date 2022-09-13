29b297b519f28283b9ee7e5557a6663e3c53d97a
Dariusz Franczak <dariuszfranczak@Dariuszs-MacBook-Pro.local>
2022-09-13 18:57:01 +0100

    navbar styled

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,50 @@
 import './navbar.scss';
 
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
+
 const Navbar = () => {
-  return <div className='navbar'>
-      Navbar
+  return (
+    <div className='navbar'>
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder='Search...' />
+          <SearchOutlinedIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlinedIcon className='icon' />
+            English
+          </div>
+          <div className="item">
+            <DarkModeOutlinedIcon className='icon' />
+          </div>
+          <div className="item">
+            <FullscreenExitOutlinedIcon className='icon' />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlinedIcon className='icon' />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon className='icon' />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ListOutlinedIcon className='icon' />
+          </div>
+          <div className="item">
+            <img src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80" alt="" className='avatar'/>
+          </div>
+        </div>
+      </div>
     </div>
+  );
 };
 
 export default Navbar;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,15 @@
-import './home.scss';
-
 import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
+
+import './home.scss';
 
 const Home = () => {
   return <div className='home'>
       <Sidebar />
-      <div className='home-container'>home</div>
+      <div className='home-container'>
+        <Navbar />
+        home
+      </div>
     </div>
 };
 
