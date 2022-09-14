ab0796616d3be9d9e802d08c0c06d91aadf7ed63
Dariusz Franczak <dariuszfranczak@Dariuszs-MacBook-Pro.local>
2022-09-14 19:15:16 +0100

    widgets added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import Sidebar from '../../components/sidebar/Sidebar'
 import Navbar from '../../components/navbar/Navbar'
+import Widget from '../../components/widget/Widget';
 
 import './home.scss';
 
@@ -8,7 +9,12 @@ const Home = () => {
       <Sidebar />
       <div className='home-container'>
         <Navbar />
-        home
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
       </div>
     </div>
 };
