c3882d55160a512beee12711f3d68e4c7e271cad
Dariusz Franczak <dariuszfranczak@Dariuszs-MacBook-Pro.local>
2022-10-26 17:17:27 +0100

    RequireAuth implemented

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   BrowserRouter,
   Routes,
   Route,
+  Navigate
 } from "react-router-dom";
 import { useContext } from "react";
 
@@ -19,28 +20,33 @@ import { DarkModeContext } from "./context/darkModeContext";
 function App() {
 
   const { darkMode } = useContext(DarkModeContext);
+  const currentUser = false;
+
+  const RequireAuth = ({children}) => {
+    return currentUser ? children : <Navigate to='/login' />;
+  }
 
   return (
     <div className={darkMode ? 'app dark' : 'app'}>
       <BrowserRouter>
         <Routes>
           <Route path='/' >
-            <Route index element={<Home />} />
             <Route path='login' element={<Login />} />
+            <Route index element={<RequireAuth><Home /></RequireAuth>} />
             <Route path='users'>
-              <Route index element={<List />}/>
-              <Route path=':userId' element={<Single />} />
+              <Route index element={<RequireAuth><List /></RequireAuth>}/>
+              <Route path=':userId' element={<RequireAuth><Single /></RequireAuth>} />
               <Route
                 path="new"
-                element={<New inputs={userInputs} title="Add New User" />}
+                element={<RequireAuth><New inputs={userInputs} title="Add New User" /></RequireAuth>}
               />
               </Route>
             <Route path='products'>
-              <Route index element={<List />}/>
-              <Route path=':productId' element={<Single />} />
+              <Route index element={<RequireAuth><List /></RequireAuth>}/>
+              <Route path=':productId' element={<RequireAuth><Single /></RequireAuth>} />
               <Route
                 path="new"
-                element={<New inputs={productInputs} title="Add New Product" />}
+                element={<RequireAuth><New inputs={productInputs} title="Add New Product" /></RequireAuth>}
               />
             </Route>
           </Route>
